c6aad1960f926db7bc29531fa978d24c0174f83e
monolive <monoliv@gmail.com>
2012-10-16 13:45:15 +0200

    Set seed : user-data need to contain seederip=yes or seederip=<ip>

--- modules/cassandra/manifests/init.pp
+++ modules/cassandra/manifests/init.pp
@@ -10,39 +10,51 @@ class cassandra::repo {
 class cassandra::install {
     $cassandra_package = ['apache-cassandra11','python26-thrift']
     package { $cassandra_package:
-        ensure      => latest,
-        require     => Class["cassandra::repo"],
+        ensure  => latest,
+        require => Class["cassandra::repo"],
     }
 }
+
  
 class cassandra::config {
     File{
         require => Class["cassandra::install"],
-        notify  => Class["cassandra::service"],
         owner   => "cassandra",
         group   => "cassandra",
-        mode    => 755
+        mode    => 644,
     }
- 
     file{"/etc/cassandra/conf/cassandra-env.sh":
-        source  => "puppet:///modules/cassandra/cassandra-env.sh";
- 
+        source  => "puppet:///modules/cassandra/cassandra-env.sh",
+        notify  => Class["cassandra::service"],
+        mode    => 755,
     }
     file{"/lib/jamm-0.2.5.jar":
         ensure  => 'link',
         target  => '/usr/share/cassandra/lib/jamm-0.2.5.jar',
     }
 }
- 
+
+class cassandra::cassandra_yaml {
+        file { "/etc/cassandra/conf/cassandra.yaml":
+                content => template("cassandra/cassandra.yaml.erb"),
+                owner   => "cassandra",
+                group   => "cassandra",
+                mode    => "644",
+                require => Class["cassandra::config"],
+                notify  => Class["cassandra::service"],
+        }
+}
+        
 class cassandra::service {
     service{"cassandra":
         ensure  => running,
         enable  => true,
-        require => Class["cassandra::config"],
+        require => Class["cassandra::cassandra_yaml"],
     }
 }
 
 class cassandra {
-    include cassandra::repo, cassandra::install, cassandra::service, cassandra::config
+    require cassandra::params
+    include cassandra::repo, cassandra::install, cassandra::service, cassandra::config, cassandra::cassandra_yaml
 }
 
